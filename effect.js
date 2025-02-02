1f47147e898369d0a420c0d73e8a2f6a54fddb50
alexs771 <alexsimmons169@gmail.com>
2025-02-02 22:09:14 +0530

    Update effect.js

--- effect.js
+++ effect.js
@@ -41,7 +41,7 @@ $(document).ready(function(){
         $('#bulb_green').addClass('bulb-glow-green-after');
         $('#bulb_pink').addClass('bulb-glow-pink-after');
         $('#bulb_orange').addClass('bulb-glow-orange-after');
-        $('body').css('backgroud-color', '#FFF');
+        $('body').css('background-color', '#FFF');
         $('body').addClass('peach-after');
         $(this).fadeOut('slow').delay(6000).promise().done(function(){
             $('#bannar_coming').fadeIn('slow');
@@ -122,24 +122,6 @@ $(document).ready(function(){
     });
 
     $('#wish_message').click(function(){
-        vw = $(window).width() / 2;
-        $('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
-        // Change balloon IDs to the new ones
-        $('#b1').attr('id', 'b11');
-        $('#b2').attr('id', 'b22');
-        $('#b3').attr('id', 'b33');
-        $('#b4').attr('id', 'b44');
-        $('#b5').attr('id', 'b55');
-        $('#b6').attr('id', 'b66');
-        $('#b7').attr('id', 'b77');
-        $('#b11').animate({top: 240, left: vw - 350}, 500);
-        $('#b22').animate({top: 240, left: vw - 250}, 500);
-        $('#b33').animate({top: 240, left: vw - 150}, 500);
-        $('#b44').animate({top: 240, left: vw - 50}, 500);
-        $('#b55').animate({top: 240, left: vw + 50}, 500);
-        $('#b66').animate({top: 240, left: vw + 150}, 500);
-        $('#b77').animate({top: 240, left: vw + 250}, 500);
-        $('.balloons').css('opacity', '0.9');
         $('.balloons h2').fadeIn(3000);
         $(this).fadeOut('slow').delay(3000).promise().done(function(){
             $('#story').fadeIn('slow');
@@ -151,34 +133,26 @@ $(document).ready(function(){
         $('.cake').fadeOut('fast').promise().done(function(){
             $('.message').fadeIn('slow');
         });
-        
+
         function msgLoop(i) {
             $("p:nth-child(" + i + ")").fadeOut('slow').delay(800).promise().done(function(){
                 i = i + 1;
                 $("p:nth-child(" + i + ")").fadeIn('slow').delay(1000);
-                if(i === 50){
-                    $("p:nth-child(49)").fadeOut('slow').promise().done(function () {
+                if(i === $(".message p").length) {
+                    $("p:nth-child(" + (i - 1) + ")").fadeOut('slow').promise().done(function () {
                         $('.cake').fadeIn('fast');
+                        $('#go_next').fadeIn('slow'); // Show "Next Surprise" button only at the end
                     });
                 } else {
                     msgLoop(i);
                 }
             });
         }
-        
         msgLoop(0);
     });
-
-    // ------------------------------------------------------------------
-    // New functionality: After a set timeout, show the "go_next" element.
-    // NOTE: Ensure an element with id "go_next" exists in your HTML.
-    setTimeout(function() {
-        $('#go_next').fadeIn(1000);
-    }, 30000); // Adjust the timeout (30000 ms = 30 seconds) based on your animation duration
 
     // Redirect when "go_next" is clicked
     $('#go_next').click(function() {
         window.location.href = 'https://www.example.com'; // Replace with your target URL
     });
-    // ------------------------------------------------------------------
 });
